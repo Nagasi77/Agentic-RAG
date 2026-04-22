993a6e6488372339bae2870f23ed74540813c6f0
Nagasi77 <2341720260@student.polinema.ac.id>
2026-04-22 22:35:28 +0700

    add rag_eng

---
+++ rag_engine.py
@@ -0,0 +1,42 @@
+import os
+from dotenv import load_dotenv
+from langchain_chroma import Chroma
+from langchain_huggingface import HuggingFaceEmbeddings
+from groq import Groq
+
+load_dotenv()
+client = Groq(api_key=os.getenv("GROQ_API_KEY"))
+
+# Inisialisasi model embedding
+embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
+vector_db = Chroma(persist_directory="./chroma_db", embedding_function=embeddings)
+
+def get_context(query):
+    docs = vector_db.similarity_search(query, k=3)
+    return "\n".join([doc.page_content for doc in docs])
+
+def agentic_rag(query):
+    context = get_context(query)
+    
+    prompt = f"""
+    Kamu adalah AI Assistant yang jujur. Gunakan konteks berikut untuk menjawab pertanyaan.
+    Konteks:
+    {context}
+
+    Pertanyaan: {query}
+
+    Aturan:
+    1. Jika jawaban tidak ada di dalam KONTEKS, katakan "Maaf, data tidak ditemukan di database". 
+    2. Jangan mengarang informasi di luar konteks yang diberikan.
+    3. Berikan jawaban yang singkat dan padat.
+    """
+    chat_completion = client.chat.completions.create(
+        messages=[
+            {"role": "system", "content": prompt}
+        ], model="llama-3-8b-instant",)
+
+    return chat_completion.choices[0].message.content
+
+if __name__ == "__main__":
+    user_query = input("Tanya dokumen kamu: ")
+    print("\nRespon AI:\n", agentic_rag(user_query))
